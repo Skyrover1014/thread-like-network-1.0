6fb62d81af7660d1333dd46ab17299e194e619ee
skyrover <maywin4755@gmail.com>
2025-04-26 22:32:49 +0800

    Collect static files including Tailwind CSS build

--- project4/settings.py
+++ project4/settings.py
@@ -157,6 +157,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme', 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
