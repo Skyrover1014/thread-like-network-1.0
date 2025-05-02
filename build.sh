618dd81734ba2b183ec4d5ff50599a57d8c621d1
skyrover <maywin4755@gmail.com>
2025-05-02 14:57:03 +0800

    Fix Render build: add npm install and build in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -o errexit
 pip install -r requirements.txt
 
 # Build Tailwind CSS
-python manage.py tailwind build
+cd theme/static_src
+npm install
+npm run build
+cd ../../
 
 # Collect static files (including built Tailwind CSS)
 python manage.py collectstatic --no-input
